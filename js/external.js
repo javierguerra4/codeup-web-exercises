42f2ce70de9af952eca2e6533151b5960e1b3b70
Javier Guerra <javier.j.guerra4@gmail.com>
2021-07-06 15:18:34 -0500

    First Javascript exercises

---
+++ js/external.js
@@ -0,0 +1,59 @@
+"use strict";
+//alert("Hi, I am A JavaScript alert!");
+
+// //var userDecision = confirm("Are you sure you don't want to buy stuff on sale?");
+// //console.log(userDecision);
+// //if (userDecision === true){
+//         alert("Wise choice!");
+// //} else {
+// //    alert("Oh no, your loss!");
+// //}
+
+// prompt
+
+// var currentSpeedInMph = 65;
+// var plannedSpeedInMph = prompt("How fast do you plan to go in mph");
+// var distanceLeftInMiles = prompt ("How many miles away are you from your destination?");
+// var timeToDestinationAtCurrentSpeed = (distanceLeftInMiles/currentSpeedInMph) * 60;
+// var timeToDestinationAtPlannedSpeed =
+//     (distanceLeftInMiles/plannedSpeedInMph) * 60;
+// var timeGain = timeToDestinationAtCurrentSpeed -
+//     timeToDestinationAtPlannedSpeed;
+// var minutesGained = Math.floor(timeGain);
+// var seconds = Math.round((timeGain - minutesGained) * 60);
+// console.log("If you drive at " + plannedSpeedInMph + " mph, you will arrive "
+// + minutesGained +  " minutes earlier");
+
+// console.log ("Hello from external JavaScript");
+// alert( "Welcome to my Website!")
+// var userFavoriteColor = prompt ("What is your favorite color?");
+// console.log(userFavoriteColor)
+//  if (userFavoriteColor === "blue"){
+//      alert( "Great, blue is my favorite color too!");
+//  }
+var lilMermaidDaysRented = prompt ("How many days did you rent Little Mermaid?");
+
+        alert( "You owe" + " " + lilMermaidDaysRented * 3 + " " + "dollars");
+
+
+var bBearDaysRented = prompt ("How many days did you rent Brother Bear?");
+
+    alert( "You owe" + " " + bBearDaysRented * 3 + " " + "dollars");
+
+
+var herculesDaysRented = prompt ("How many days did you rent Hercules?");
+
+    alert( "You owe" + " " + herculesDaysRented * 3 + " " + "dollars");
+
+//  var  google = prompt ("How many hours did you work at Google?");
+//     if (google === "6"){
+//        alert( "Your pay is" + " " + 6 * 400 + " " + "dollars");
+//     }
+// var  amazon = prompt ("How many hours did you work at Amazon?");
+// if (amazon === "4"){
+//     alert( "Your pay is" + " " + 4 * 380 + " " + "dollars");
+// }
+// var  facebook = prompt ("How many hours did you work at Facebook?");
+// if (facebook === "10"){
+//     alert( "Your pay is" + " " + 10 * 350 + " " + "dollars");
+// }
