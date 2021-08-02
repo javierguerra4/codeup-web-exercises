dca3695f0e0b052fc630d6326ac1c591ba953380
Javier Guerra <javier.j.guerra4@gmail.com>
2021-08-02 01:49:54 -0500

    Created a while loop

---
+++ js/break_and_continue.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var userInput = prompt("Please enter an odd number between 1-50.")
+
+while (parseFloat(userInput) >= 50 || parseFloat(userInput) % 2 === 0){
+    prompt("That's not an odd number or a number between 1 and 50! Try again.");
+    userInput++;
+}
+
