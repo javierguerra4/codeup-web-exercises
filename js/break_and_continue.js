0b7b3b06f15b1d5b3f29440f0ada092891cf198f
Javier Guerra <javier.j.guerra4@gmail.com>
2021-08-02 08:32:47 -0500

    Prompt exercise is complete

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -5,5 +5,8 @@ var userInput = prompt("Please enter an odd number between 1-50.")
 while (parseFloat(userInput) >= 50 || parseFloat(userInput) % 2 === 0){
     prompt("That's not an odd number or a number between 1 and 50! Try again.");
     userInput++;
+    if (parseFloat(userInput) < 50 || (parseFloat(userInput) % 2) !== 0){
+        break;
+    }
 }
 
