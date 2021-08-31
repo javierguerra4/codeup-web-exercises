717769ef54f6bcb02f638f729ccfa96f8248963f
Javier Guerra <javier.j.guerra4@gmail.com>
2021-08-30 21:59:43 -0500

    Fetch

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -39,7 +39,8 @@ const users = [
 ];
 
 
-//Problem 1
+//Problem 2
+// .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 // const atLeastThree = users.filter(function (n) {
 //     return n.languages.length >= 3;
 // });
@@ -48,8 +49,22 @@ const atLeastThree = users.filter(n => n.languages.length >= 3 );
 
 
 
-//Problem 2 Use .map to create an array of strings where each element is a user's email address
+//Problem 3
+// Use .map to create an array of strings where each element is a user's email address
 
 const userEmails = users.map( n => n.email);
 
+
+
+//Problem 4
+// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+
+const totalYears = users.reduce((total, person) => {
+    return total + person.yearsOfExperience;
+}, 0);
+
+
+
+//Problem 5
+// Use .reduce to get the longest email from the list of users.
 
---
+++ js/promises.js
@@ -0,0 +1,6 @@
+"use strict"
+
+
+
+fetch(https://api.github.com/users/`${username}`/events, {headers: {'Authorization': 'token GIT_HUB_TOKEN'}})
+// https://api.github.com/users/`${username}`/events
